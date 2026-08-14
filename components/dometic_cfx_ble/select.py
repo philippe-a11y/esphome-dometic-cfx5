378117f54b7bea17f881c9729bfd660916ac0bae
philippe-a11y <philippe-a11y@users.noreply.github.com>
2026-08-14 20:12:24 +0000

    Make select.py character-identical to the proven wila91 version
    The 'expected a list' config error pointed at the select schema.
    Replace select.py with the exact upstream-fork version known to
    compile, ruling out any subtle schema difference.

--- components/dometic_cfx_ble/select.py
+++ components/dometic_cfx_ble/select.py
@@ -6,22 +6,19 @@ from . import dometic_cfx_ble_ns, CONF_DOMETIC_CFX_BLE_ID, entity_schema
 
 DEPENDENCIES = ["dometic_cfx_ble"]
 
-DometicCfxBleSelect = dometic_cfx_ble_ns.class_(
-    "DometicCfxBleSelect", select.Select, cg.PollingComponent
-)
+DometicCfxBleSelect = dometic_cfx_ble_ns.class_("DometicCfxBleSelect", select.Select, cg.PollingComponent)
 
-CONFIG_SCHEMA = entity_schema("select").extend(
-    select.select_schema(DometicCfxBleSelect)
-).extend({
+CONFIG_SCHEMA = entity_schema("select").extend(select.select_schema(DometicCfxBleSelect)).extend({
     cv.GenerateID(): cv.declare_id(DometicCfxBleSelect)
 })
-
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    # Pre-populate the dropdown options (battery protection levels).
+    
+    # Pre-populate the dropdown menu options
     await select.register_select(var, config, options=["Low", "Medium", "High"])
+
     parent = await cg.get_variable(config[CONF_DOMETIC_CFX_BLE_ID])
     cg.add(var.set_parent(parent))
     cg.add(var.set_topic(config[CONF_TYPE]))
